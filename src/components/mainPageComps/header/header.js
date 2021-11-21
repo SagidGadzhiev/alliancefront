45302e5d8cf2992e2f9d42c5f10440e40df834fd
SagidGadzhiev <sagidkg36@gmail.com>
2021-11-22 00:04:10 +0600

    commit

--- src/components/mainPageComps/header/header.js
+++ src/components/mainPageComps/header/header.js
@@ -90,22 +90,29 @@ const Header = () => {
                     <label className='searching__form__label'>
                         <input className='searching__form__label__input' type="text" placeholder='Искать...'
                                onChange={setSearchingHandler} value={searching} title='Поиск товаров'/>
-                        {/*<button onClick={() => {*/}
-                        {/*    dispatch(removeSearching());*/}
-                        {/*    dispatch(getCurrentPage(1))*/}
-                        {/*}} style={{*/}
-                        {/*    display: `${searching.length === 0 ? 'none' : 'block'}`*/}
-                        {/*}} className='searching__form__label__deleteBtn' type="button">&#10006;</button>*/}
-                        <Link
-                            // onClick={() => dispatch(getCurrentPage(1))}
-                            to={`/search=${searching}`} className='searching__form__label__btn' type="submit">
+                        <button onClick={() => {
+                            dispatch(removeSearching());
+                            dispatch(getCurrentPage(1))
+                        }} style={{
+                            display: `${searching.length === 0 ? 'none' : 'block'}`
+                        }} className='searching__form__label__deleteBtn' type="button">&#10006;</button>
+                        <Link onClick={() => dispatch(getCurrentPage(1))} to={`/search=${searching}`}
+                              className='searching__form__label__btn' type="submit">
+                            {/*<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search"*/}
+                            {/*     className="svg-inline--fa fa-search fa-w-16 searching__form__label__btn__pic"*/}
+                            {/*     role="img"*/}
+                            {/*     xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">*/}
+                            {/*    <path fill=""*/}
+                            {/*          d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>*/}
+                            {/*</svg>*/}
+
                             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search"
-                                 className="svg-inline--fa fa-search fa-w-16 searching__form__label__btn__pic"
-                                 role="img"
+                                 className="svg-inline--fa fa-search fa-w-16 searching__form__label__btn__pic" role="img"
                                  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                 <path fill=""
                                       d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
                             </svg>
+
                         </Link>
                     </label>
                 </form>
